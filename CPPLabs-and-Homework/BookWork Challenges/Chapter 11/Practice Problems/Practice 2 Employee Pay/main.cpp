066bdbad8dfc0ca8c4fbd9dc2a7ef126f6064176
Garrett Jackson <105180907+aceofroses@users.noreply.github.com>
2025-03-20 10:17:43 -0500

    Added practice problems

---
+++ CPPLabs-and-Homework/BookWork Challenges/Chapter 11/Practice Problems/Practice 2 Employee Pay/main.cpp
@@ -0,0 +1,45 @@
+// This program demonstrates partially innitialized strutured variables
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+struct EmployeePay
+{
+    string name;
+    int empNum;
+    double payRate;
+    double hours;
+    double grossPay;
+};
+
+int main()
+{
+    EmployeePay employee1 = {"Betty Ross", 141, 18.75};
+    EmployeePay employee2 = {"Jill Sandburg", 142, 17.50};
+
+    cout << fixed << showpoint << setprecision(2);
+
+    // Calculate pay for employee1
+    cout << "Name: " << employee1.name << "\n"
+         << "Employee Number: " << employee1.empNum << "\n"
+         << "Enter the hours worked by this employee: " << endl;
+    cin >> employee1.hours;
+
+    employee1.grossPay = employee1.hours * employee1.payRate;
+
+    cout << "Gross Pay: " << employee1.grossPay << endl << endl;
+
+    // Calculate pay for employee2
+// Calculate pay for employee1
+    cout << "Name: " << employee2.name << "\n"
+         << "Employee Number: " << employee2.empNum << "\n"
+         << "Enter the hours worked by this employee: " << endl;
+    cin >> employee2.hours;
+
+    employee2.grossPay = employee2.hours * employee2.payRate;
+
+    cout << "Gross Pay: " << employee2.grossPay << endl << endl;
+    return 0;
+
+}
