be541731aa9e7cefb77c81e2c6d19a8130007fcf
Garrett Jackson <105180907+aceofroses@users.noreply.github.com>
2025-04-10 13:36:57 -0500

    Updated labs

--- CPPLabs-and-Homework/Labs/Lab 10 More About Classes/Lab Problems/Problem_1_Car_Instrument_Simulator/FuelGauge.h
+++ CPPLabs-and-Homework/Labs/Lab 10 More About Classes/Lab Problems/Problem_1_Car_Instrument_Simulator/FuelGauge.h
@@ -2,52 +2,62 @@
 #define FUELGAUGE_H
 #include <iostream>
 
-
-
 class FuelGauge
 {
     private:
         int fuelGallons;
     public:
-
         //Set the starting value of the gallons in the tank
         FuelGauge(int startingGallons)
         {
-            fuelGallons = 0;
+            fuelGallons = startingGallons;
         }
 
-        //Returns the number of gallons
-        FuelGauge getGallons() const
+        //Returns the number of gallons in the tank
+        int getGallons() const
         {
             return fuelGallons;
         }
 
-        //Allows for fuel to be added back to the tank, back to the max of 15
+        //Allows for fuel to be added back to the tank, back to the max of 15, taking gallons as a parameter
         void addToTank(int gallons)
         {
-            fuelGallons += gallons;
-            cout << "Test";
+            //
+            for(int i = 0; i < 15; i++)
+            {    
+                // Checks if the private member plus the parameter is less than 15
+                if(fuelGallons + gallons <= 15)
+                {
+                    // Adds the parameter to the private member
+                    fuelGallons += gallons;
+                }
+                else if(fuelGallons == 0)
+                {
+                    fuelGallons = 15;
+                }
+            }
         }
 
         //Function for fuel to be subtracted from the car's tank
         void useFuel()
         {
+            //Decrement by one as long as the fuel is above zero
             if(fuelGallons > 0)
+            {
                 fuelGallons--;
+            }
+            // Once the tank reaches zero, state the tank is empty
             if(fuelGallons == 0)
-                cout << "Tank empty. Please refill" << endl;
+            {
+                std::cout << "Tank empty. Please refill" << std::endl;
+            }
+            
         }
-
         //Display's the remaining gas when called
         void report()
         {
-            cout << "Remaining Gas: " << fuelGallons << endl;
+            std::cout << "Remaining Gas: " << fuelGallons << std::endl;
         }
-
-
-
-
-
 };
 
 #endif
--- CPPLabs-and-Homework/Labs/Lab 10 More About Classes/Lab Problems/Problem_1_Car_Instrument_Simulator/Odometer.h
+++ CPPLabs-and-Homework/Labs/Lab 10 More About Classes/Lab Problems/Problem_1_Car_Instrument_Simulator/Odometer.h
@@ -12,31 +12,47 @@ class Odometer
         int mileTracker;
     public:
 
+        //Constructor for the mileage tracking and mileage object
         Odometer(int mileage)
         {
-            
+            this->mileage = mileage;
+            mileTracker = 0;
 
         }
 
-        void advance(int mileTracker, FuelGauge &gauge)
+        Odometer& advance(int mileTracker, FuelGauge &gauge)
         {
-            for(int i = 0; i < 999999; i++)
+            //Check if miles travelled is equal to 999999, if it is, set it back to zero.
+            // If not, keep incremeting
+            for(int i = 0; i < mileTracker; i++)
             {
-                if(mileage < 999999)
-                {
-                    mileage += 1;
-                }
-                else if(mileage >= 999999)
-                {
-                    mileage = 0;
-                }
+              // Continuously increment the mileage
+              this->mileage++;
+
+              //Once the mileage hits the cap, reset it back to zero
+              if(this->mileage > 999999)
+              {
+                this->mileage = 0;
+              }
+
+              //Continuously increment the tracker
+              this->mileTracker++;
+
+              //Checks when the mileage equals 24, then utilizes the useFuel function from the FuelGauge Class
+              // Then sets the tracker back to 0
+              if(this->mileTracker == 24)
+              {
+                gauge.useFuel();
+                this->mileTracker = 0;
+              }
             }
         
+            return *this;
         }
 
         void report()
         {
-            cout << "Mileage: " << mileage << endl;
+            std::cout << "Mileage: " << mileage << std::endl;
         }
 
 
